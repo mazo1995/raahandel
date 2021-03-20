2f5a401e85ea3bf5813d238d66ceaf39468bc882
Slurpee <mazo1995@gmail.com>
2021-03-20 17:12:21 +0100

    Update scripts.js

--- test/scripts.js
+++ test/scripts.js
@@ -1,11 +1,11 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function firstFunction() {
+function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginRight = "250px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function secondFunction() {
+  function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginRight = "0";
   }
